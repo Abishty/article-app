98c544c84e068842692fb646e372514559427035
ab-bisht <18abhishek.b@gmail.com>
2019-12-04 13:05:13 +0530

    labels change according to user login data

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { signUp } from '../../store/actions/authActions';
 
 class SignUp extends React.Component {
 
@@ -19,11 +20,11 @@ class SignUp extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.signUp(this.state);
     }
 
     render() {
-        const { auth } = this.props
+        const { auth, authError } = this.props
         if(!auth.uid){
             return (
                 <div className="container">
@@ -46,7 +47,11 @@ class SignUp extends React.Component {
                         <input type='password' id='password' onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Signup</button>                    </div>
+                        <button className="btn pink lighten-1 z-depth-0">Signup</button>
+                        <div className="red-text center">
+                            {authError ? <p>{authError}</p> : null}
+                        </div>
+                    </div>
                 </form>
             </div>
             )
@@ -62,13 +67,14 @@ class SignUp extends React.Component {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        signUp: (newUser) => dispatch(signUp(newUser))
     }
 }
 
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -8,12 +8,12 @@ const SignedInLinks = (props) => {
     const handleClick = (e) => {
         props.signOut()
     }
-
+    const { initials } = props;
     return (
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
             <li><a onClick={handleClick}>Log Out</a></li>
-            <li><NavLink to='/' className="btn btn-floating pink lighten-1">AB</NavLink></li>
+            <li><NavLink to='/' className="btn btn-floating pink lighten-1">{initials}</NavLink></li>
         </ul>
     )
 }
@@ -21,7 +21,7 @@ const SignedInLinks = (props) => {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-
+        initials: state.firebase.profile.initials
     }
 }
 
