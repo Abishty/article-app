de271e5be77f1b1f0a40c05e41e0dc34a3133b9f
ab-bisht <18abhishek.b@gmail.com>
2019-12-05 12:26:59 +0530

    users notification added in db

--- functions/index.js
+++ functions/index.js
@@ -26,3 +26,17 @@ exports.projectCreated = functions.firestore
 
     return createNotification(notification);
   });
+
+exports.userJoined = functions.auth.user().onCreate( user => {
+    return admin.firestore().collection('users').doc(user.uid).get().then( doc => {
+        const newUser = doc.data();
+        const notification = {
+            content: 'Joined the party',
+            user: `${newUser.firstName} ${newUser.lastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+    } );
+} );
